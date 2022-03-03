c9cb667486ecb5226ce7c5e2307cfa5838cdcee1
affanashraf <affanashraf120@gmail.com>
2022-03-03 13:04:45 +0500

    Update index.tsx

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -2,17 +2,21 @@ import Jobs from '../../components/Jobs'
 
 export async function getStaticProps() {
 	try {
-		// const result = await fetch(`${process.env.HOST_URL}/api/jobs?sort=hotJob`, {
-		// 	method: 'GET',
-		// })
-		// const foo = await result.json()
+		const result = await fetch(`${process.env.HOST_URL}/api/jobs?sort=hotJob`, {
+			method: 'GET',
+		})
+		const foo = await result.json()
+		return { props: {
+			count:0,
+			jobs:[],
+			data: JSON.stringify((foo))
+		} }
+	} catch (e) {
+		console.log(e)
 		return { props: {
 			count:0,
 			jobs:[]
 		} }
-	} catch (e) {
-		console.log(e)
-		return { props: {} }
 	}
 }
 

e38a3f5c79bec3846d2eb6de46aff3b530706a68
affanashraf <affanashraf120@gmail.com>
2022-03-03 12:58:36 +0500

    test

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -2,11 +2,14 @@ import Jobs from '../../components/Jobs'
 
 export async function getStaticProps() {
 	try {
-		const result = await fetch(`${process.env.HOST_URL}/api/jobs?sort=hotJob`, {
-			method: 'GET',
-		})
-		const foo = await result.json()
-		return { props: {...foo} }
+		// const result = await fetch(`${process.env.HOST_URL}/api/jobs?sort=hotJob`, {
+		// 	method: 'GET',
+		// })
+		// const foo = await result.json()
+		return { props: {
+			count:0,
+			jobs:[]
+		} }
 	} catch (e) {
 		console.log(e)
 		return { props: {} }
